dee866e2cc612f7f700ec3ff2a33c18e69c432bb
root <re.thyerry698@gmail.com>
2023-08-26 13:11:00 -0300

    Filtragem de itens

--- src/components/layout/ItemCard.jsx
+++ src/components/layout/ItemCard.jsx
@@ -3,7 +3,7 @@ import styles from "../styles/ItemCard.module.css"
 function ItemCard({id, image, price, name, filter}) {
     return(
         <div className={styles.card_Item}>
-            <img src=".././media/shirt-1.jpg" alt="Imagem do item" />
+            <img src={image} alt="Imagem do item" />
             <h4>{name}</h4>
             <span>R$ {price}</span>
             <button>Comprar</button>
